2a27b1ec740e8e9541b38e52d2a9086f0471e8d4
pray@falabella.cl <pavelray@administrators-MacBook-Pro.local>
2022-04-19 19:45:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Helmet from 'react-helmet';
 
 function App() {
+  const getCountry = () =>{
+
+    switch(process.env.REACT_APP_TENANT){
+      case 'cl': 
+        return 'Chille';
+      case 'pe':
+        return 'Peru'
+      default:
+        return ''
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Helmet>
+      <title>Welcome App </title>
+    </Helmet>
+    <div>
+      <div>
+        Welcome to <b>{process.env.REACT_APP_NAME}</b>
+      </div>
+      <div>Country : {getCountry() }</div>
     </div>
+    </>
   );
 }
 
